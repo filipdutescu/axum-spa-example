a163f5ad92c1554ab3870cd0feef55c9ebc7401d
Filip Dutescu <filip.dutescu@gmail.com>
2022-01-15 14:53:45 +0200

    chore: setup docker
    Signed-off-by: Filip Dutescu <filip.dutescu@gmail.com>

--- be/src/main.rs
+++ be/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::SocketAddr;
+
+use axum::{routing::get, Router};
+use axum_debug::debug_handler;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+
+    let app = Router::new().route("/api", get(greet));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
+
+    tracing::info!("Server listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+#[debug_handler]
+async fn greet() -> &'static str {
+    "Hello World!"
 }
